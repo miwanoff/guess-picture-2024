df0f2d647b0f24e7b883b17e56a3102117fa49c2
miwanoff <marina.iva.noffa@gmail.com>
2024-12-29 14:57:56 +0200

    Add event

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 window.onload = function () {
-  let image1 = document.getElementById("one");
-  image1.onclick = showImageOne;
-  let image2 = document.getElementById("two");
-  image2.onclick = showImageTwo;
-}
+  //   let image1 = document.getElementById("one");
+  //   image1.onclick = showImageOne;
+  //   let image2 = document.getElementById("two");
+  //   image2.onclick = showImageTwo;
+  let images = document.getElementsByTagName("img");
+  for (let i = 0; i < images.length; i++) {
+    images[i].onclick = showAnswer;
+  }
+};
 
-function showImageOne() {
-  let image = document.getElementById("one");
-  image.src = "images/one.jpg";
-}
+// function showImageOne() {
+//   let image = document.getElementById("one");
+//   image.src = "images/one.jpg";
+// }
 
-function showImageTwo() {
-  let image = document.getElementById("two");
-  image.src = "images/two.jpg";
+// function showImageTwo() {
+//   let image = document.getElementById("two");
+//   image.src = "images/two.jpg";
+// }
+
+function showAnswer(event) {
+  let image = event.target;
+  let name = image.id;
+  name = name + ".jpg";
+  image.src = "images/" + name;
 }
